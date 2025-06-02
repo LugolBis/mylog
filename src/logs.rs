02c6d2b7f9b97a26d659675875c266409d720262
LugolBis <ld.desmares@gmail.com>
2025-06-02 23:27:42 +0200

    Some useful functions implemented at the airport

---
+++ src/logs.rs
@@ -0,0 +1,50 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+#[allow(unused)]
+pub fn write_log(content: String) {
+    match  OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("logs.txt")
+    {
+        Ok(mut file) => { let _ = file.write_all(content.as_bytes()); },
+        Err(error) => { println!("Error when try to write log : {:?}",error); }
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($format_str:expr) => {{
+        let msg = format!("[INFO] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        $crate::logs::write_log(msg);
+    }};
+    ($format_str:expr, $($arg:tt)*) => {{
+        let msg = format!("[INFO] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        $crate::logs::write_log(msg);
+    }};
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($format_str:expr) => {{
+        let msg = format!("[WARNING] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        $crate::logs::write_log(msg);
+    }};
+    ($format_str:expr, $($arg:tt)*) => {{
+        let msg = format!("[WARNING] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        $crate::logs::write_log(msg);
+    }};
+}
+
+#[macro_export]
+macro_rules! error {
+    ($format_str:expr) => {{
+        let msg = format!("[ERROR] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        $crate::logs::write_log(msg);
+    }};
+    ($format_str:expr, $($arg:tt)*) => {{
+        let msg = format!("[ERROR] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        $crate::logs::write_log(msg);
+    }};
+}
