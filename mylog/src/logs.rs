802cd20039c2fd194d1c075e7f3751af67b88c1f
LugolBis <ld.desmares@gmail.com>
2025-06-03 22:37:53 +0200

    implementing timestamp support

--- mylog/src/logs.rs
+++ mylog/src/logs.rs
@@ -1,9 +1,11 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::ffi::CStr;
+use libc::{localtime, strftime, time_t, tm};
 
 const LOG_FILE_PATH: &str = "logs.txt";
 
-#[allow(unused)]
 pub fn write_log(content: String) {
     match OpenOptions::new()
         .append(true)
@@ -19,14 +21,40 @@ pub fn write_log(content: String) {
     }
 }
 
+pub fn get_time() -> String {
+    let system_time = SystemTime::now();
+    let duration = system_time.duration_since(UNIX_EPOCH).unwrap();
+    let timestamp = duration.as_secs() as time_t;
+
+    unsafe {
+        let tm_ptr: *mut tm = localtime(&timestamp);
+        let mut buffer = [0u8; 100];
+
+        let format = "%Y-%m-%d %H:%M:%S\0";
+        let len = strftime(
+            buffer.as_mut_ptr() as *mut _,
+            buffer.len(),
+            format.as_ptr() as *const _,
+            tm_ptr,
+        );
+
+        if len > 0 {
+            let cstr = CStr::from_ptr(buffer.as_ptr() as *const _);
+            String::from(cstr.to_str().unwrap_or("~Date~"))
+        } else {
+            String::from("~Date~")
+        }
+    }
+}
+
 #[macro_export]
 macro_rules! info {
     ($format_str:expr) => {{
-        let msg = format!("[INFO] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        let msg = format!("[{}] [INFO] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), $format_str);
         $crate::logs::write_log(msg);
     }};
     ($format_str:expr, $($arg:tt)*) => {{
-        let msg = format!("[INFO] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        let msg = format!("[{}] [INFO] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), format!($format_str, $($arg)*));
         $crate::logs::write_log(msg);
     }};
 }
@@ -34,11 +62,11 @@ macro_rules! info {
 #[macro_export]
 macro_rules! warn {
     ($format_str:expr) => {{
-        let msg = format!("[WARNING] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        let msg = format!("[{}] [WARNING] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), $format_str);
         $crate::logs::write_log(msg);
     }};
     ($format_str:expr, $($arg:tt)*) => {{
-        let msg = format!("[WARNING] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        let msg = format!("[{}] [WARNING] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), format!($format_str, $($arg)*));
         $crate::logs::write_log(msg);
     }};
 }
@@ -46,11 +74,11 @@ macro_rules! warn {
 #[macro_export]
 macro_rules! error {
     ($format_str:expr) => {{
-        let msg = format!("[ERROR] [{}:{}] : {}\n", file!(), line!(), $format_str);
+        let msg = format!("[{}] [ERROR] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), $format_str);
         $crate::logs::write_log(msg);
     }};
     ($format_str:expr, $($arg:tt)*) => {{
-        let msg = format!("[ERROR] [{}:{}] : {}\n", file!(), line!(), format!($format_str, $($arg)*));
+        let msg = format!("[{}] [ERROR] [{}:{}] : {}\n", $crate::logs::get_time(), file!(), line!(), format!($format_str, $($arg)*));
         $crate::logs::write_log(msg);
     }};
 }
